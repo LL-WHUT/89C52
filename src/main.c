6c94644c5a19e9a415cdd2bdb0141f87cf784ef0
LL-WHUT <342874@whut.edu.cn>
2023-07-03 19:27:07 +0800

    ds18b20温度测量

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@ void main()
 {
     lcd1602_init();
     lcd1602_show_string(0, 0, "xx-dev.cn");
-    lcd1602_show_string(0, 1, "19870326.cn");
+    lcd1602_show_string(0, 1, "ling");
 
 
     while (1) {
