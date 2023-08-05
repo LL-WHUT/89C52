757cbbc07c805a5e6ace3343360c3b9a26a22254
LL-WHUT <342874@whut.edu.cn>
2023-08-05 16:19:49 +0800

    定时器中断示例

--- src/main.c
+++ src/main.c
@@ -1,34 +1,33 @@
 #include <reg52.h>
-#include <stdlib.h>
-#include <ds18b20.h>
-#include <smg_display.h>
-void int0() interrupt 0
+
+void time0_init(void)
 {
-    delay_10us(1000);
-    if (P32 == 0) 
+    TMOD |= 0x01;//选择定时器0，工作模式1
+    TH0 = 0xfc;TL0 = 0x18;//给定时器赋初值1ms
+    ET0 = 1;//打开定时器0中断允许
+    EA  = 1;//打开总中断
+    TR0 = 1;//打开定时器0
+}
+
+void time0() interrupt 1
+{
+    static u16 i;
+    TH0=0xfc;
+    TL0=0x18;
+    i++;
+    if(i==1000)
     {
-        P20 = !P20;
+        i=0;
+        P20=!P20;
     }
 }
+
 void main()
 {
-    IT0=1;
-    EX0=1;
-    EA=1;
-    float tm  = 0;
-    u16 tmp   = 0;
-    u8 str[5] = {'\0'};
-    while (1) 
+    time0_init();
+    while (1)
     {
-        ds18b20_init();
-        tm     = ds18b20_read_temperture();
-        tm     = 10 * tm;
-        tmp    = tm;
-        str[0] = tmp / 100 + '0';
-        str[1] = (tmp / 10) % 10 + '0';
-        str[2] = '.';
-        str[3] = tmp % 10 + '0';
-        str[4] = '\0';
-        Smg_display(str, 100);
+        ;
     }
+    
 }
