b8cbe72a096eb1077da08fe0f36b69ad037a3c47
LL-WHUT <342874@whut.edu.cn>
2023-08-12 18:56:11 +0800

    AT24C02测试

--- src/main.c
+++ src/main.c
@@ -2,12 +2,27 @@
 #include <keyscan.h>
 #include <iic.h>
 #include <at24c02.h>
+#include <smg_display.h>
+
+#define EEPROM_ADDRESS 0 // 定义数据存入EEPROM的起始地址
+
 void main()
 {
-    while (1) {
-        if (key_scan(0)) {
-            P20 = !P20;
-            delay_ms(500);
+    u8 dat='0';
+    u8 temp='0';
+    while (1)
+    {
+        if(key_scan(1))
+        {
+            dat++;
+        }
+        at24c02_write_one_data(EEPROM_ADDRESS,dat);
+        temp=at24c02_read_one_data(EEPROM_ADDRESS);
+        Smg_display(&temp,100);//数码管显示有点毛病，不过验证AT24C02足够了
+        if(dat=='9')
+        {
+            dat='0';
         }
     }
+    
 }
